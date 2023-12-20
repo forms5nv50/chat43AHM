cd074ea474383f6f9db46ac88fb887777a3ee498
forms5nv50 <152238731+forms5nv50@users.noreply.github.com>
2023-12-20 01:28:35 -0600

    jsjs

---
+++ init.js
@@ -0,0 +1,11 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyA94cex1uOViNbKIncgJHvpXVOOuWPO64I",
+  authDomain: "chat43mavr.firebaseapp.com",
+  projectId: "chat43mavr",
+  storageBucket: "chat43mavr.appspot.com",
+  messagingSenderId: "51489921839",
+  appId: "1:51489921839:web:c30cce36900d74df8d1fda"
+    });
